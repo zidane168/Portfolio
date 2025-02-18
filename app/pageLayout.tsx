3cec4eb620df0d95025ec816ff026ad8eccce6cd
Vi Ly <vi.lh@>
2025-02-18 17:38:06 +0700

    add button scroll

--- app/pageLayout.tsx
+++ app/pageLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 import TitleUI from './Component/UI/TitleUI';
 import LanguageSection from "./Component/Section/Language/LanguageSection";
@@ -16,9 +16,44 @@ interface PageLayoutProps {
 const PageLayout: React.FC<PageLayoutProps> = ({  children }) => {
   const color = useStore((state: any) => state.color);  // using Store/store.tsx
   const date = new Date();
+
+  useEffect(() => { 
+    function scrollWin() {
+      window.scrollTo({top: 0, behavior: 'smooth'});
+    }
+
+    // Adding event listener to the button
+    const myBtn = document.getElementById('myBtn');
+    if (myBtn) {
+      myBtn.addEventListener('click', scrollWin);
+    }
+
+    // Displaying the button when scrolling down
+    window.onscroll = function() {
+      if (myBtn) {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          myBtn.style.display = 'block';
+        } else {
+          myBtn.style.display = 'none';
+        }
+      }
+    };
+
+    // Initial hide of the button
+    if (myBtn) {
+      myBtn.style.display = 'none';
+    }
+
+    // Clean up the event listener
+    return () => {
+      if (myBtn) {
+        myBtn.removeEventListener('click', scrollWin);
+      }
+    }
+  }, [])
   
   return (
- 
+     
    
       <div className={ `p-[20px] border-[2px] rounded-lg m-[20px] `} style={ { borderColor: color, boxShadow: `10px 10px 100px ${color}` } } >
         <div className="text-sm flex flex-col space-y-[20px] lg:flex-row lg:space-x-[20px] h-full">
@@ -63,6 +98,7 @@ const PageLayout: React.FC<PageLayoutProps> = ({  children }) => {
           </div> 
 
           <div className="w-[100%] ">
+            <button className="btn-scroll-to-top" type="button" id="myBtn">Top</button>
             { children }
           </div> 
 
@@ -73,3 +109,5 @@ const PageLayout: React.FC<PageLayoutProps> = ({  children }) => {
 };
 
 export default PageLayout;
+
+ 
