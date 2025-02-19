38505665e2f0331178e95b43d64f8ae7ae125d6f
Vi Ly <vi.lh@>
2025-02-19 10:00:58 +0700

    add scale

--- app/pageLayout.tsx
+++ app/pageLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import TitleUI from './Component/UI/TitleUI';
 import LanguageSection from "./Component/Section/Language/LanguageSection";
@@ -17,45 +17,33 @@ const PageLayout: React.FC<PageLayoutProps> = ({  children }) => {
   const color = useStore((state: any) => state.color);  // using Store/store.tsx
   const date = new Date();
 
-  useEffect(() => { 
-    function scrollWin() {
-      window.scrollTo({top: 0, behavior: 'smooth'});
+  // button 
+  const [ showScrollToTop, setShowScrollToTop ] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 300) {
+      setShowScrollToTop(true);
+    } else {
+      setShowScrollToTop(false);
     }
+  }
 
-    // Adding event listener to the button
-    const myBtn = document.getElementById('myBtn');
-    if (myBtn) {
-      myBtn.addEventListener('click', scrollWin);
-    }
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
-    // Displaying the button when scrolling down
-    window.onscroll = function() {
-      if (myBtn) {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          myBtn.style.display = 'block';
-        } else {
-          myBtn.style.display = 'none';
-        }
-      }
-    };
-
-    // Initial hide of the button
-    if (myBtn) {
-      myBtn.style.display = 'none';
-    }
-
-    // Clean up the event listener
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      if (myBtn) {
-        myBtn.removeEventListener('click', scrollWin);
-      }
+      window.removeEventListener('scroll', handleScroll);
     }
   }, [])
-  
+ 
   return (
      
    
       <div className={ `p-[20px] border-[2px] rounded-lg m-[20px] `} style={ { borderColor: color, boxShadow: `10px 10px 100px ${color}` } } >
+
         <div className="text-sm flex flex-col space-y-[20px] lg:flex-row lg:space-x-[20px] h-full">
           
           <div className="lg:w-[25%]  flex-col text-center border-r-[2px] pr-[20px]" style={{ borderRightColor: color }}>
@@ -97,12 +85,22 @@ const PageLayout: React.FC<PageLayoutProps> = ({  children }) => {
             </div>
           </div> 
 
-          <div className="w-[100%] ">
-            <button className="btn-scroll-to-top" type="button" id="myBtn">Top</button>
+          <div className="w-[100%] "> 
             { children }
           </div> 
 
         </div>
+
+        {showScrollToTop && (
+          <button
+            onClick={ scrollToTop }
+            className={ `fixed p-3 text-white transition rounded-full shadow-lg bottom-5 right-5 hover:scale-125 ` }
+            style={{ backgroundColor: color }}
+          >
+            Top
+          </button>
+        )}
+        
       </div> 
  
   );
